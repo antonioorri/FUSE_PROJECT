fa24064ef95f4d40e1878e38d3df8e1217e189ee
domopeacemaker <juandomopeacemakerlovepaw@gmail.com>
2022-06-15 00:00:41 +0200

    corrección de errores y mejoras, versión aumentada a 1.1.2

--- fusesinho.h
+++ fusesinho.h
@@ -14,38 +14,38 @@
 
 
 typedef struct superbloque {
-	char datablocks[block_size*100];		//total number of data blocks
-	char data_bitmap[105];      			//array of data block numbers that are available
-	char inode_bitmap[105];   				//array of inode numbers that are available
+	char datablocks[block_size*100];		//número de bloques totales
+	char data_bitmap[105];      			//array de bloques de datos disponibles
+	char inode_bitmap[105];   				//array de inodos disponibles
 } superbloque;
 
 typedef struct inode {
-	int datablocks[16];            //data block number that the inode points to
+	int datablocks[16];            //bloque de datos que apunta el inodo
 	int number;
-	int blocks;                    //==number of blocks the particular inode points to
-	//int link;                    //==number of links
-	int size;                      //==size of file/directory
+	int blocks;                    
+	//int link;                    /
+	int size;                      //tamaño del archivo o directorio
 } inode;
 
 typedef struct filetype {
 	int valid;
 	char test[10];
 	char path[100];
-	char name[100];           //name
-	inode *inum;              //inode number
+	char name[100];           //nombre
+	inode *inum;              //número de inodo
 	struct filetype ** children;
 	int num_children;
 	int num_links;
 	struct filetype * parent;
-	char type[20];                  //==file extension
-	mode_t permissions;		        // Permissions
+	char type[20];                  //extension del archivo
+	mode_t permissions;		        // permisos
 	uid_t user_id;		            // userid
 	gid_t group_id;		            // groupid
-	time_t a_time;                  // Access time
-	time_t m_time;                  // Modified time
-	time_t c_time;                  // Status change time
-	time_t b_time;                  // Creation time
-	off_t size;                     // Size of the node
+	time_t a_time;                  // tiempo de accesos
+	time_t m_time;                  // fecha de modificación
+	time_t c_time;                  // hora del estado
+	time_t b_time;                  // hora de la creación
+	off_t size;                     // tamaño del nodo
 
 	int datablocks[16];
 	int number;
